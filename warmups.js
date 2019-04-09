3fdc36938a07d62d5dcd5f67adccad0972ef1f94
annika <annikaslund@gmail.com>
2019-04-09 12:17:12 -0700

    Add solution for first 3 problems

---
+++ warmups.js
@@ -0,0 +1,46 @@
+// Product of Nums
+// Write a function that finds the product of an array of numbers:
+
+function product(arr){
+    if (arr.length === 0) return 1;
+
+    return arr.pop() * product(arr);
+}
+
+// Longest Word
+// Given a list of words, return the length of the longest:
+
+function longestWord(wordList, length=0){
+    if (wordList.length === 0) return length;
+
+    let word = wordList.pop()
+    if (word.length > length){
+        length = word.length;
+    }
+
+    return longestWord(wordList, length)
+}
+
+// Every Other Character
+// Write a function that returns a string of every other character:
+
+function everyOtherCharacter(str, i=0, output=""){
+    if (i === str.length) return output;
+
+    if (i % 2 === 0){
+        output += str[i]
+    }
+
+    return everyOtherCharacter(str, i+1, output)
+}
+
+// Is Palindrome?
+// Write a function that returns true/false depending on whether passed-in string is a palindrome:
+
+function isPalindrome(str, i=str.length-1, compStr=""){
+    if (i < 0) return (str === compStr);
+
+    compStr += str[i];
+    return isPalindrome(str, i-1, compStr);
+}
+
